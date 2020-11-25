e90215c59038e8894b2cacc5aa9eb672d66eacaf
Javier Lizarraga <javierlizarraga.b@hotmail.com>
2020-11-24 17:35:09 -0700

    Added php files

---
+++ data/database.php
@@ -0,0 +1,17 @@
+<?php
+    // Database credentials
+    $servername = "den1.mysql4.gear.host";
+    $username   = "iotjavo";
+    $password   = "	Vq5XV9!!O4RQ";
+    $dbname     = "iotjavo";
+    $tbname     = "sensor";
+
+    // Create database connection
+    $conn=mysqli_connect($servername,$username,$password,"$dbname");
+    
+    // Check Connection
+    if(!$conn){
+        die('Could not Connect My Sql:' .mysql_error());
+    }
+    //echo "[MySQL] Connected successfully <br/>";
+?>
